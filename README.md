8461bd791f408694bdf128e972114709e969ea76
ltalting <74326531+ltalting@users.noreply.github.com>
2022-06-23 22:08:26 -0400

    initial project, gitignore, and readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # overwatch-stats-merger
+
+### 1.0.0
+## Added
+ - initial blank project files
---
+++ overwatch-stats-merger/Form1.cs
@@ -0,0 +1,10 @@
+namespace overwatch_stats_merger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
